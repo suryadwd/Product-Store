3e6488822ff570ddb4db8821a472a75a3f703a38
Suryakant <surajdwivedi644@gmail.com>
2024-11-03 13:59:43 +0530

    Done

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express, { json } from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import path from "path"
+import productRoutes from "./routes/product.route.js"
+
+const app = express();
+
+dotenv.config();
+const __dirname = path.resolve()
+app.use(express.json())
+app.use("/api/products",productRoutes)
+
+const PORT = process.env.PORT 
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+})
+
+}
+
+app.listen(5000, ()=> {
+    connectDB()
+    console.log("server is live at http://localhost:" + PORT);
+    
+})
+
+
+
+//server.js
+
+
